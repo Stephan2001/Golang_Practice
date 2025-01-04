6b8d86ed45bd0e5374315cc99314aef24193e3ae
ST10219213 <moolmans20013@gmail.com>
2025-01-04 08:48:58 +0200

    slices

---
+++ slices/slices.go
@@ -0,0 +1,13 @@
+package slices
+
+func SumArray(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	return nil
+}

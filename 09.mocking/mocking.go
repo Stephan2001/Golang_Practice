ed80b82baf61e13f107fea0f13684f4d57825e9a
ST10219213 <stephan.moolman.dev@gmail.com>
2025-04-13 18:15:33 +0200

    Mocking

--- 09.mocking/mocking.go
+++ 09.mocking/mocking.go
@@ -4,16 +4,40 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func main() {
-	Countdown(os.Stdout)
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
 }
 
-func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		sleeper.Sleep()
 	}
 
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
